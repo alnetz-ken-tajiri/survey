f8660396cf13e69ddc7de6c599d5d248a012fa2a
ken tajiri <k.tajiri@alnetz.co.jp>
2025-02-20 14:13:20 +0900

    migration6

---
+++ app/api/admin/notifications/route.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+export async function GET(request: NextRequest) {
+    
+    const notifications = await prisma.notification.findMany({
+        
+    });
+
+    return NextResponse.json(notifications);
+}
